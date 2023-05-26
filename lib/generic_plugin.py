f7f974732b0422b5afb83016e08a557e3f4218a2
Markus Sander <m.sander@mr-daten.de>
2023-05-26 11:47:08 +0200

    new functions
    - added 'check_threshold', a generic function that returns codes for
      warning/critical/ok. Supports ">" and "<" comparsion
    - added 'update_rc', a generic function that sets the return code to the
      worset value

--- lib/generic_plugin.py
+++ lib/generic_plugin.py
@@ -1,11 +1,46 @@
 """common functions for check-plugins"""
 
-def rcstring( rc ):
+def rcstring( rc, b='(' ):
+    brace = {
+            '(': [ '(', ')' ],
+            '[': [ '[', ']' ],
+            '{': [ '{', '}' ]
+            }
+
     rcstring = {
-        0: "(OK)",
-        1: "(WARNING)",
-        2: "(CRITICAL)",
-        3: "(UNKNOWN)"
+        0: f"{brace[b][0]}OK{brace[b][1]}",
+        1: f"{brace[b][0]}WARNING{brace[b][1]}",
+        2: f"{brace[b][0]}CRITICAL{brace[b][1]}",
+        3: f"{brace[b][0]}UNKNOWN{brace[b][1]}"
         }
     return(rcstring[rc])
+
+
+def check_threshold( metric, warn_thres, crit_thres, method='gt' ):
+    rc_return=0
+    if method == 'gt':
+        if  crit_thres  != None and metric > crit_thres :
+            rc_return = 2
+        elif warn_thres != None and metric > warn_thres :
+            rc_return = 1
+        else:
+            rc_return = 0
+
+    elif method == 'lt':
+        if crit_thres   != None and metric < crit_thres :
+            rc_return = 2
+        elif warn_thres != None and metric < warn_thres :
+            rc_return = 1
+        else:
+            rc_return = 0
+    else:
+        True
+
+    return( rc_return )
+
+def update_rc( new, old ):
+    if new > old:
+        return( new )
+    else:
+        return( old )
 
