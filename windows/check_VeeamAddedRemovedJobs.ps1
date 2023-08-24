edfcfd76d4e93a9e1ec2b3a793445c73cbc7a011
Charly Kupke <c.kupke@mr-daten.de>
2023-08-24 07:41:41 +0000

    Add new Check for Veeam Job changes

---
+++ windows/check_VeeamAddedRemovedJobs.ps1
@@ -0,0 +1,61 @@
+###  Veeam added/removed Job Checker  ###
+###   (c) MR-Daten - Charly Kupke     ###
+###           Version 1.0             ###
+### ### ### ### #### #### ### ### ### ###
+
+try {
+    $veeamjobs = Get-VBRJob | Sort LogNameMainPart
+} catch {
+    Write-Host "(WARNING) Can't get VBR Jobs"
+    ;exit (1)
+}
+
+$WorkPath = $env:SystemDrive + "\ProgramData\icinga2\var\temp"
+If(!(Test-Path $WorkPath))
+{
+    ## Create Dir if not exists
+    New-Item -ItemType Directory -Force -Path $WorkPath
+}
+cd $WorkPath
+$filePath = "$WorkPath\check_VeeamAddedRemovedJobs.txt"
+
+$currentJobs = @()
+Foreach ($veeamjob in $veeamjobs) {
+    $veeam_jobname = $veeamjob.LogNameMainPart
+    $currentJobs += $veeam_jobname
+}
+
+$previousJobs = Get-Content -Path $filePath
+
+$differences = Compare-Object $currentJobs $previousJobs
+$currentJobs | Out-File -FilePath $filePath
+
+if ($differences -eq $null) {
+    $OutputContent = '(OK) Keine neuen / entfernte Jobs gefunden'
+    $ExitCode = 0
+}
+else {
+    $newJobsContent = ""
+    $oldJobsContent = ""
+
+    Foreach ($difference in $differences) {
+        If ($difference.SideIndicator -eq '=>') {
+            $newJobsContent += $difference.InputObject + "`n"
+        }
+        If ($difference.SideIndicator -eq '<=') {
+            $oldJobsContent += $difference.InputObject + "`n"
+        }
+    }
+    if ($newJobsContent -ne '') {
+        $OutputContent = '(WARNING) Jobs seit letztem Check hinzugefügt:' + "`n"
+        $OutputContent += $newJobsContent + "`n"
+    }
+    if ($oldJobsContent -ne '') {
+        $OutputContent += '(WARNING) Jobs seit lettem Check entfernt:' + "`n"
+        $OutputContent += $oldJobsContent
+    }
+    $ExitCode = 1
+}
+
+Write-Host $OutputContent
+;exit ($ExitCode)
