15321b716fba148fb0aef51691db8ddb60e84f30
Charly Kupke <c.kupke@mr-daten.de>
2023-08-30 08:32:00 +0000

    Disabled Job handling changed to remove PS errors

--- windows/check_VeeamBackupJob.ps1
+++ windows/check_VeeamBackupJob.ps1
@@ -96,7 +96,7 @@ Foreach ($veeamjob in $veeamjobs) {
     $veeam_state = $veeamjob.GetLastState()
     if (($exclusivejob -and $exclusivejob -contains $veeam_jobname) -or ($ignorejob -and $ignorejob -notcontains $veeam_jobname) -or (!$exclusivejob -and !$ignorejob)) {
         $OutputCount_Jobs = $OutputCount_Jobs + 1
-        if (!(($veeam_jobtype -eq 'Hyper-V Backup Copy') -or ($veeam_jobtype -eq 'VMware Backup Copy'))) {
+        if (!(($veeam_jobtype -eq 'Hyper-V Backup Copy') -or ($veeam_jobtype -eq 'VMware Backup Copy')) -and $veeam_schedule) {
             $veeam_jobhistory = $veeamjobshistory | ?{$_.OrigJobName -eq $veeam_jobname}
             if ($veeam_jobhistory[0].State -eq 'Stopped') {
                 $veeam_jobruntime = New-TimeSpan -Start $veeam_jobhistory[0].CreationTime -End $veeam_jobhistory[0].EndTime
@@ -195,6 +195,11 @@ Foreach ($veeamjob in $veeamjobs) {
                     }
                 }
             }
+        }
+        elseif (!($veeam_schedule)) {
+            $OutputContent += "(OK) Job: $veeam_jobname; Scheduled: disabled"
+            $OutputContent += "`n"
+            $OutputCount_OK = $OutputCount_OK + 1
         }
         else {
             if ($veeam_result -eq 'Success') {
